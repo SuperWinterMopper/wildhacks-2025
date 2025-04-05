a715ce7523e76bc0370e8f6d2869d0d0ccc9d042
Acton Healy <afkhealy@gmail.com>
2025-04-05 17:55:20 -0500

    made everything on one page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
 import DistanceSelector from "./initial-selection";
-import ButtonPress from "./button-press";
 
 export default function Home() {
   return (  
     <div className="flex justify-center my-10">
-      <div className="w-5/6 h-screen border-gray-500 border-2">
         <DistanceSelector/>
-        <ButtonPress/>
-      </div>
     </div>
   );
 }
