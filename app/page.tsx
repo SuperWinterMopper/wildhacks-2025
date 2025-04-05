09e273ed84c0da4df51886510bb04374336f0641
Juliane Guo <jessicaguo934@gmail.com>
2025-04-05 14:16:42 -0500

    button added in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import DistanceSelector from "./InitialSelection";
+import ButtonPress from "./ButtonPress";
 
 export default function Home() {
   return (  
     <div className="flex justify-center">
       <div className="w-5/6 h-screen border-gray-500 border-2">
         <DistanceSelector/>
+        <ButtonPress/>
       </div>
     </div>
   );
