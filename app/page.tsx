3dff45d5e804fc6e0b142304700e509fd7037fcb
Acton Healy <afkhealy@gmail.com>
2025-04-05 16:09:19 -0500

    dark mode switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import DistanceSelector from "./initial-selection";
 import ButtonPress from "./button-press";
+import ModeToggle from "./mode-toggle";
 
 export default function Home() {
   return (  
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="w-5/6 h-screen border-gray-500 border-2">
         <DistanceSelector/>
         <ButtonPress/>
+        <ModeToggle/>
       </div>
     </div>
   );
