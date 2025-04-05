ab79e9480b44748b3a5339a958d7d5ce99f2dbc8
Acton Healy <afkhealy@gmail.com>
2025-04-05 17:58:47 -0500

    about to pull

--- app/initial-selection.tsx
+++ app/initial-selection.tsx
@@ -119,7 +119,7 @@ export default function DistanceSelector() {
       </div>
         <div className="flex justify-center items-center w-full">
             <Button variant="outline" className="!border-2 !border-green-500 font-medium flex justify-center p-4 cursor-pointer w-70 h-15 text-3xl"> 
-            Find me routes
+            Find me routes 
             </Button>
         </div>
     </div>
