594dc70203c59f9c50c30eea19e9cce4e8897c38
Juliane Guo <jessicaguo934@gmail.com>
2025-04-05 15:32:13 -0500

    more maps

---
+++ app/map/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SimpleMap from './simple-map';
+
+export default function MapPage() {
+  return (
+    <div>
+      <h1>Mapped Routes</h1>
+      <SimpleMap/>
+    </div>
+  );
+}
